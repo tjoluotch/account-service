433366eb96f1b6e969c05c1c4c647e01e28a08fd
TJ Oluotch <trevorjoluotch@gmail.com>
2020-03-14 00:42:08 +0000

    added HTTP method assertion to router handlers

--- multiplexer.go
+++ multiplexer.go
@@ -3,12 +3,13 @@ package main
 import (
 	. "consul-service/internal/api"
 	"github.com/gorilla/mux"
+	"net/http"
 )
 
 func ServerMux(service *Service) (*mux.Router, error) {
 	router := mux.NewRouter()
 	router.Use(service.LoggingMiddleware)
-	router.HandleFunc("/api/health", service.HealthHandler)
-	router.HandleFunc("/api/payment", service.PaymentHandler)
+	router.HandleFunc("/api/health", service.HealthHandler).Methods(http.MethodGet)
+	router.HandleFunc("/api/payment", service.PaymentHandler).Methods(http.MethodPut)
 	return router, nil
 }
