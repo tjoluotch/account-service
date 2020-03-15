d3f800a49ff906a706735bfa22c9b291fbbfc17e
TJ Oluotch <trevorjoluotch@gmail.com>
2020-03-15 20:39:11 +0000

    Added data model for request

--- internal/api/payment.go
+++ internal/api/payment.go
@@ -5,4 +5,7 @@ import "net/http"
 func (service *Service) PaymentHandler(resp http.ResponseWriter, req *http.Request) {
 	logger := service.Logger
 	logger.Info("payment")
+	//	TODO: implelment handler
+	//	check for empty body - return bad request
+	//	decode request payload into Data type if
 }
---
+++ internal/models/data.go
@@ -0,0 +1,6 @@
+package models
+
+type Payment struct {
+	Amount     int32
+	SenderBank string
+}
